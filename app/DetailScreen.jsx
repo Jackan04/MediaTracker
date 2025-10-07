48ec1e6b03e50e1bcecce1c2ad6df9750668569d
Jackan04 <jacob.asker@icloud.com>
2025-10-07 17:21:20 +0200

    Refactor: temporarily removed loading and error message logic; will later extract into a reusable component shared across the app Fix: added rounding to rating on meta info card to display a single decimal place only

--- app/DetailScreen.jsx
+++ app/DetailScreen.jsx
@@ -11,150 +11,140 @@ import { deleteItem, insertItem, isItemSaved } from "./server/queries";
 import globalStyles from "./utils/globalStyles";
 import { COLORS, SIZES } from "./utils/theme";
 
-export default function DetailScreen(){
-    const params = useLocalSearchParams();
-    const [item, setItem] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [isSaved, setIsSaved] = useState()
-    const router = useRouter()
+export default function DetailScreen() {
+  const params = useLocalSearchParams();
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isSaved, setIsSaved] = useState();
+  const router = useRouter();
 
-    useEffect(() => {
-        const displayDetails = async () => {
-            try {
-                setLoading(true);
-                const result = await getItemDetails(params.tmdb_id, params.media_type);
-                setItem(result);
-                console.log(result)
-            } catch (error) {
-                console.error('Error fetching details:', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        if (params.tmdb_id && params.media_type) {
-            displayDetails();
-        }
-    }, [params.tmdb_id, params.media_type]);
-
-    useEffect(() => {
-        const checkIfSaved = async () => {
-            if (item && item.tmdb_id) {
-                try {
-                    const savedStatus = await isItemSaved(item.tmdb_id);
-                    setIsSaved(savedStatus);
-                } catch (error) {
-                    console.error('Error checking if item is saved:', error);
-                }
-            }
-        };
-        checkIfSaved();
-    }, [item]); 
-
-    const handleDelete = async () => {
-    Alert.alert(
-        "Remove from Library",
-        "Are you sure you want to remove this item from your library?",
-        [
-            {
-                text: "Cancel",
-                style: "cancel"
-            },
-            {
-            text: "Remove",
-            style: "destructive",
-            onPress: async () => {
-                try {
-                    await deleteItem(item.tmdb_id);
-                    setIsSaved(false);
-                } catch (error) {
-                    console.error('Error deleting item:', error);
-                }
-            }
-            }
-        ]
-    )
-}
-    const handleSave = async () => {
-        try {
-            await insertItem(item);
-            setIsSaved(true);
-        } catch (error) {
-            console.error('Error saving item:', error);
-        }
+  useEffect(() => {
+    const displayDetails = async () => {
+      try {
+        setLoading(true);
+        const result = await getItemDetails(params.tmdb_id, params.media_type);
+        setItem(result);
+      } catch (error) {
+        console.error("Error fetching details:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    if (loading) {
-        return (
-            <SafeAreaView style={globalStyles.container} edges={['left', 'right', 'bottom', 'top']}>
-                <Header title="Details" />
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Loading...</Text>
-                </View>
-            </SafeAreaView>
-        );
+    if (params.tmdb_id && params.media_type) {
+      displayDetails();
     }
+  }, [params.tmdb_id, params.media_type]);
 
-    if (!item) {
-        return (
-            <SafeAreaView style={globalStyles.container} edges={['left', 'right', 'bottom', 'top']}>
-                <Header title="Details" />
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Error loading details</Text>
-                </View>
-            </SafeAreaView>
-        );
+  useEffect(() => {
+    const checkIfSaved = async () => {
+      if (item && item.tmdb_id) {
+        try {
+          const savedStatus = await isItemSaved(item.tmdb_id);
+          setIsSaved(savedStatus);
+        } catch (error) {
+          console.error("Error checking if item is saved:", error);
+        }
+      }
+    };
+    checkIfSaved();
+  }, [item]);
+
+  const handleDelete = async () => {
+    Alert.alert(
+      "Remove from Library",
+      "Are you sure you want to remove this item from your library?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Remove",
+          style: "destructive",
+          onPress: async () => {
+            try {
+              await deleteItem(item.tmdb_id);
+              setIsSaved(false);
+            } catch (error) {
+              console.error("Error deleting item:", error);
+            }
+          },
+        },
+      ]
+    );
+  };
+  const handleSave = async () => {
+    try {
+      await insertItem(item);
+      setIsSaved(true);
+    } catch (error) {
+      console.error("Error saving item:", error);
     }
+  };
 
+  return (
+    <SafeAreaView
+      style={globalStyles.container}
+      edges={["left", "right", "bottom", "top"]}
+    >
+      <View>
+        <Button
+          style={styles.backButton}
+          text="Back"
+          onPress={() => router.back()}
+        ></Button>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.top}>
+          <Image
+            style={styles.poster}
+            source={{ uri: item.poster_path }}
+            placeholder="https://via.placeholder.com/120x180?text=No+Image"
+            contentFit="cover"
+          />
 
-    return(
-        <SafeAreaView style={globalStyles.container} edges={['left', 'right', 'bottom', 'top']}>
-            <View>
-            <Button style={styles.backButton} text="Back" onPress={() => router.back()}></Button>
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.top}>
-                    <Image 
-                        style={styles.poster}
-                        source={{ uri: item.poster_path }} 
-                        placeholder="https://via.placeholder.com/120x180?text=No+Image"
-                        contentFit="cover"
-                    />
+          <Text style={[globalStyles.title]}>{item.title}</Text>
+          <MetaInfoRow
+            year={item.year}
+            runtime={
+              item.media_type === "movie"
+                ? Math.round((item.runtime_minutes / 60) * 10) / 10 + "h"
+                : ""
+            } // Round to one decimal  and display an empty string if the value is zero
+            genre={JSON.parse(item.genres_json)
+              .slice(0, 3)
+              .map((genre) => genre.name)
+              .join(", ")} // Parse JSON, limit to 3 genres
+            rating={Math.round(item.vote_average * 10) / 10}
+          />
 
-                    <Text style={[globalStyles.title]}>{item.title}</Text>
-                    <MetaInfoRow
-                        year={item.year}
-                        runtime={ item.media_type === "movie" ? Math.round((item.runtime_minutes / 60) * 10) / 10 + "h" : ""} // Round to one decimal  and display an empty string if the value is zero
-                        genre={JSON.parse(item.genres_json).slice(0, 3).map(genre => genre.name).join(', ')} // Parse JSON, limit to 3 genres
-                        rating={item.vote_average}
-                    />
-                    
-                    <Button 
-                        text={isSaved ? "Remove from Library" : "Save to Library"} 
-                        onPress={isSaved ? handleDelete : handleSave} 
-                        buttonTextColor = {isSaved ? COLORS.redDark : COLORS.blueDark}
-                        buttonBgColor = {isSaved ? COLORS.redLight : COLORS.blueLight}
-                    />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
+          <Button
+            text={isSaved ? "Remove from Library" : "Save to Library"}
+            onPress={isSaved ? handleDelete : handleSave}
+            buttonTextColor={isSaved ? COLORS.redDark : COLORS.blueDark}
+            buttonBgColor={isSaved ? COLORS.redLight : COLORS.blueLight}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    top:{
-        alignSelf: "center",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: SIZES.sm,
-    },
-    poster: {
-        width: 200,
-        height: 300,
-        borderRadius: 8,
-    },
-    backButton:{
-        flex: 0,
-        alignSelf: "flex-start",
-    }
-})
+  top: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: SIZES.sm,
+  },
+  poster: {
+    width: 200,
+    height: 300,
+    borderRadius: 8,
+  },
+  backButton: {
+    flex: 0,
+    alignSelf: "flex-start",
+  },
+});
--- app/components/MetaInfoRow.jsx
+++ app/components/MetaInfoRow.jsx
@@ -1,46 +1,47 @@
-import { StyleSheet, Text, View } from "react-native"
-import { Ionicons } from '@expo/vector-icons'
-import { COLORS, FONT_SIZES, SIZES } from "../utils/theme"
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { COLORS, FONT_SIZES, SIZES } from "../utils/theme";
 
-
-export default function MetaInfoRow(props){
-
-    return(
-       <View style={styles.container}>
-        <Text style={styles.text}>{props.year}</Text>
-        <Text style={styles.text}>{props.runtime}</Text>
-        <Text style={styles.text}>{props.genre}</Text>
-        <View style={styles.ratingContainer}>
-            <Text style={styles.text}>{props.rating}</Text>
-            <Ionicons name="star" size={SIZES.sm} color={COLORS.yellowDark}></Ionicons>
-        </View>
-       </View>
-    )
+export default function MetaInfoRow(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.year}</Text>
+      <Text style={styles.text}>{props.runtime}</Text>
+      <Text style={styles.text}>{props.genre}</Text>
+      <View style={styles.ratingContainer}>
+        <Text style={styles.text}>{props.rating}</Text>
+        <Ionicons
+          name="star"
+          size={SIZES.sm}
+          color={COLORS.yellowDark}
+        ></Ionicons>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        justifyContent: "center",
-        alignContent: "center",
-        alignSelf: "flex-start",
-        flexWrap: "wrap",
-        gap: SIZES.sm,
-        paddingVertical: SIZES.xs,
-        paddingHorizontal: SIZES.sm,
-        backgroundColor: COLORS.greyLight,
-        borderWidth: 1,
-        borderColor: COLORS.border,
-        borderRadius: SIZES.radius,
-    },
-    text:{
-        color: COLORS.textPrimary,
-        fontSize: FONT_SIZES.sm,
-    },
-    ratingContainer:{
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 4,
-    },
-
-})
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignSelf: "flex-start",
+    flexWrap: "wrap",
+    gap: SIZES.sm,
+    paddingVertical: SIZES.xs,
+    paddingHorizontal: SIZES.sm,
+    backgroundColor: COLORS.greyLight,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: SIZES.radius,
+  },
+  text: {
+    color: COLORS.textPrimary,
+    fontSize: FONT_SIZES.sm,
+  },
+  ratingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+});
